4a28e65b07ec516f27da71163c6f7b4df79948c6
suyeon kim <88700545+sueyon032@users.noreply.github.com>
2023-10-17 02:21:12 +0900

    spring boot 프로젝트 생성

---
+++ src/main/java/prepare/prepare_project/PrepareProjectApplication.java
@@ -0,0 +1,13 @@
+package prepare.prepare_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrepareProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrepareProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/prepare/prepare_project/PrepareProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package prepare.prepare_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrepareProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
