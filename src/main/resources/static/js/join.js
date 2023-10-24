ec6ce8662187121d4cd6febb73989ab799989344
suyeon kim <88700545+sueyon032@users.noreply.github.com>
2023-10-25 06:22:22 +0900

    join화면 구현

---
+++ src/main/resources/static/js/join.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded",function(){
+   var loginButton= document.getElementById("joinBtn");
+   loginButton.onclick=function(){
+    window.location.href="index.html";
+   };
+});
