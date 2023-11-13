1ce5e1626b43c7ca7b35f8cdf6a3ef5e3b8b837d
suyeon kim <88700545+sueyon032@users.noreply.github.com>
2023-11-14 05:06:19 +0900

    feat: db 연결

--- src/main/java/prepare/prepare_project/controller/LikeController.java
+++ src/main/java/prepare/prepare_project/controller/LikeController.java
@@ -1,12 +1,28 @@
 package prepare.prepare_project.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import prepare.prepare_project.entity.CountryInfoEntity;
+import prepare.prepare_project.service.CountryInfoService;
+
+import java.util.List;
 
 @Controller
 public class LikeController {
+//    @GetMapping("/like")
+//    public String showIndexPage(){
+//        return "like";
+//    }
+
+    @Autowired
+    private CountryInfoService countryInfoService;
+
     @GetMapping("/like")
-    public String showIndexPage(){
-        return "like";
+    public String showCountryInfo(Model model) {
+        List<CountryInfoEntity> countryInfoList = countryInfoService.getAllCountryInfo();
+        model.addAttribute("countryInfoList", countryInfoList);
+        return "like"; // Thymeleaf 템플릿의 이름
     }
 }
